44f693c5bd2c57e29a8a3ecee80a70ac975069a5
Priyadharshini M <mpriyadharshinicse23@gmail.com>
2025-03-19 21:03:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var on_btn= document.getElementById("on");
+var off_btn=document.getElementById("off");
+var img=document.getElementById("bulb");
+
+function bulb_on()
+{
+    img.src="https://img.freepik.com/free-vector/realistic-lit-light-bulb-isolated_1284-41774.jpg?ga=GA1.1.854551793.1734338740&semt=ais_hybrid";
+} 
+function bulb_off()
+{
+    img.src="https://img.freepik.com/free-vector/realistic-light-bulb-isolated_1284-41741.jpg?ga=GA1.1.854551793.1734338740&semt=ais_hybrid";
+}
